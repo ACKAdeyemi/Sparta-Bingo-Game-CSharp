604a5c10c9d0d277980004db528c55d3ccf3a9b3
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-08-15 21:18:17 +0100

    Basic setup for console app complete on virtual machine. Attemping to load onto personal Native Windows Machine.

---
+++ BingoConsoleApp/BingoConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BingoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Virtual Machine");
+
+            Console.ReadLine();
+        }
+    }
+}
