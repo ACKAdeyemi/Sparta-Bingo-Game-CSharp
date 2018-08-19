062119d214054f5ecba7fe9e01a840d8fa94794c
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-08-20 00:57:55 +0100

    Attempt failed.

--- BingoConsoleApp/BingoConsoleApp/Program.cs
+++ BingoConsoleApp/BingoConsoleApp/Program.cs
@@ -18,34 +18,40 @@ namespace BingoConsoleApp
                 playerNumbers = { }
             };
 
+            List<int> pickedNumbers = new List<int>();
+            int rolledNumbersCounter = 10;
+            bool keepPlaying = true;
+
+            PlayGame(player, pickedNumbers, rolledNumbersCounter, keepPlaying);
+            CheckPlayerWin(pickedNumbers, player, rolledNumbersCounter, keepPlaying);
+
+            // BELOW is to prevent application ended after player wins/final number roll
+            //Console.ReadLine();
+        }
+
+        public static void PlayGame(Player playerClass, List<int> list, int counter, bool playState)
+        {
             // BELOW allows player to set their own name 
             Console.WriteLine("\nHi Player 1. \nWelcome to BINGO by ACKA. \nWhat is your name?");
-            player.name = Console.ReadLine();
-            Console.WriteLine("\nHi " + player.name + ", thanks for joining! Have a great game!");
+            playerClass.name = Console.ReadLine();
+            Console.WriteLine("\nHi " + playerClass.name + ", thanks for joining! Have a great game!");
 
-            Console.WriteLine("\nGenerating " + player.name + "'s numbers...");
+            Console.WriteLine("\nGenerating " + playerClass.name + "'s numbers...");
             // BELOW confirms array items can be listed
-            Console.WriteLine(player.name + "'s numbers are:");
-            foreach (int number in player.playerNumbers)
+            Console.WriteLine(playerClass.name + "'s numbers are:");
+            foreach (int number in playerClass.playerNumbers)
             {
                 Console.Write(number + ", ");
             }
 
             Console.WriteLine("\n\nLET'S PLAY BINGO!");
 
-            List<int> pickedNumbers = new List<int>();
-
-            RollNumbers(pickedNumbers, player);
-
-            // BELOW is to prevent application ended after player wins/final number roll
-            Console.ReadLine();
+            RollNumbers(list, playerClass, counter, playState);
         }
 
-        public static void gameState()
+        public static void GameState(List<int> list, int loopCounter, bool playState)
         {
-            bool keepPlaying = true;
-
-            while (keepPlaying)
+            while (playState)
             {
                 Console.WriteLine("Would you like to play again? \nEnter 1 for YES and 2 for NO");
                 string entry = Console.ReadLine();
@@ -61,7 +67,8 @@ namespace BingoConsoleApp
                     }
                     else if (num == 2)
                     {
-                        keepPlaying = false;
+                        playState = false;
+                        //loopCounter = list.Count; // stops the loop from telling user to press spacebar after they win
                         Console.WriteLine("Thank you for playing! Goodbye.");
                         return;
                     }
@@ -79,7 +86,7 @@ namespace BingoConsoleApp
             }
         }
 
-        public static void CheckPlayerWin(List<int> list, Player playerClass)
+        public static void CheckPlayerWin(List<int> list, Player playerClass, int loopCounter, bool playState)
         {
             List<int> checkWinList = new List<int>();
             // BELOW adds player's numbers to a list for comparison
@@ -113,41 +120,44 @@ namespace BingoConsoleApp
                 Console.WriteLine("\nBINGO!");
                 Console.WriteLine(playerClass.name + " wins the GAME!");
                 Console.WriteLine("GAME END");
-                gameState();
+                GameState(checkWinList, loopCounter, playState);
             }
         }
 
         // BELOW inserts random numbers into a list
-        public static void RollNumbers(List<int> list, Player playerClass)
+        public static void RollNumbers(List<int> list, Player playerClass, int loopCounter, bool playState)
         {
             Random randomNumber = new Random();
             int number;
 
             // loop iterations needs to be one lower than max random num to work
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < loopCounter; i++)
             {
-                do
+                while (playState)
                 {
-                    number = randomNumber.Next(1, 11);
-                } while (list.Contains(number));
-                list.Add(number);
-                Console.WriteLine("\nPress SPACEBAR to roll a number...");
-                if (Console.ReadKey().Key == ConsoleKey.Spacebar)
-                {
-                    Console.WriteLine("\nThe next rolled number is " + number);
-
-                    Console.WriteLine("\n" + playerClass.name + "'s numbers are:");
-                    foreach (int playerNumber in playerClass.playerNumbers)
+                    do
                     {
-                        Console.Write(playerNumber + ", ");
-                    }
-
-                    Console.WriteLine("\nThese are the rolled numbers so far:");
-                    foreach (int num in list)
+                        number = randomNumber.Next(1, 11);
+                    } while (list.Contains(number));
+                    list.Add(number);
+                    Console.WriteLine("\nPress SPACEBAR to roll a number...");
+                    if (Console.ReadKey().Key == ConsoleKey.Spacebar)
                     {
-                        Console.Write(num + ", ");
+                        Console.WriteLine("\nThe next rolled number is " + number);
+
+                        Console.WriteLine("\n" + playerClass.name + "'s numbers are:");
+                        foreach (int playerNumber in playerClass.playerNumbers)
+                        {
+                            Console.Write(playerNumber + ", ");
+                        }
+
+                        Console.WriteLine("\nThese are the rolled numbers so far:");
+                        foreach (int num in list)
+                        {
+                            Console.Write(num + ", ");
+                        }
+                        CheckPlayerWin(list, playerClass, loopCounter, playState);
                     }
-                    CheckPlayerWin(list, playerClass);
                 }
             }
         }
@@ -169,3 +179,10 @@ namespace BingoConsoleApp
 // Need to add press enter for new ball funtionality
 // ABOVE - needs to include info on players numbers and current rolled number list ("All rolled numbers so far:")
 // Need to incliude a way for the game to be restart (use a bool and say "while play = true, do method" - take content out of main and put into seperate method and then maybe wrap the while loop in that method... call it 'playBingo')
+
+// NOTE:
+// Press spacebar issue still present (player wins game but when trying to quit by typing 2, 
+// if there are still balls to be rolled, 
+// the game will continue to ask the player to roll until the last has been rolled, 
+// then presenting "Thanks for playing" message)
+// PLAN - commit this work, then copy and paste code from 'press-enter-function' here to try something different
